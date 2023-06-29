e6176fd0eeb8129713d25de941171db0c835016a
ayanjdeka <ayanjdeka@gmail.com>
2023-06-28 23:24:15 -0500

    Adding another type of mnist-model

---
+++ python_implementations/mlp-mnist/mlp-mnist.py
@@ -0,0 +1,55 @@
+'''
+This is an implementation of the RNN MNIST digits classification,
+with a SGD optimizer
+'''
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Activation, Dense, Dropout
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten
+from tensorflow.keras.utils import to_categorical, plot_model
+from tensorflow.keras.datasets import mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+num_labels = len(np.unique(y_train))
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+image_size = x_train.shape[1]
+input_size = image_size * image_size
+x_train = np.reshape(x_train, [-1, input_size])
+x_train = x_train.astype('float32') / 255
+x_test = np.reshape(x_test, [-1, input_size])
+x_test = x_test.astype('float32') / 255
+
+# network parameters
+batch_size = 128
+hidden_units = 256
+dropout = 0.45
+
+# model is a 3-layer MLP with ReLU and utilizes a dropout after each layer
+model = Sequential()
+model.add(Dense(hidden_units, input_dim=input_size))
+model.add(Activation('relu'))
+model.add(Dropout(dropout))
+model.add(Dense(hidden_units))
+model.add(Activation('relu'))
+model.add(Dropout(dropout))
+model.add(Dense(num_labels))
+model.add(Activation('softmax'))
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+# train the network
+model.fit(x_train, y_train, epochs=20, batch_size=batch_size)
+
+_, acc = model.evaluate(x_test,
+                        y_test,
+                        batch_size=batch_size,
+                        verbose=0)
+print("\nTest accuracy: %.1f%%" % (100.0 * acc))
